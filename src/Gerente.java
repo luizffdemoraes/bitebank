8e21181446d850800e6cdfdcd074ab81948dfac9
Luiz Felipe <lffm1994@gmail.com>
2023-01-11 01:11:32 -0300

    Aplicando Polimorfismo

--- src/Gerente.java
+++ src/Gerente.java
@@ -19,6 +19,7 @@ public class Gerente extends Funcionario {
 
     // Rescrita
     public double getBonificacao() {
+        System.out.println("Chamando o método bonificacao do GERENTE");
         return super.getBonificacao() + super.getSalario();
     }
 }
