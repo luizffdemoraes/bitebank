8244593c89550ad9952acd6b5d4398c776dfcdc8
Luiz Felipe <lffm1994@gmail.com>
2023-01-13 00:33:39 -0300

    Implementando tributaveis

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,10 +1,21 @@
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements  Tributavel{
     public ContaCorrente(int agencia, int numero) {
         super(agencia, numero);
     }
 
     @Override
     public boolean saca(double valor) {
-        return super.saca(valor + 0.20);
+        double valorASacar = valor + 0.2;
+        return super.saca(valorASacar);
+    }
+
+    @Override
+    public void deposita(double valor) {
+        super.deposita(valor);
+    }
+
+    @Override
+    public double getValorImposto() {
+        return getSaldo() * 0.01;
     }
 }
