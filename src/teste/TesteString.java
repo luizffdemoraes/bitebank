fd0b1aeb93c070d8ec43a4566495ac0f8618e562
Luiz Felipe <lffm1994@gmail.com>
2023-01-17 08:04:48 -0300

    Metodos da classe String

--- src/teste/TesteString.java
+++ src/teste/TesteString.java
@@ -4,14 +4,45 @@ public class TesteString {
 
     public static void main(String[] args) {
 
-        String nome = "Alura"; // Forma correta de inicialização -> sintaxe object literal string é uma classe
+
+        String vazio = " Alura ";
+        String outroVazio = vazio.trim();
+
+        System.out.println(outroVazio);
+
+        // String vazio = "";
+        // System.out.println(vazio.isEmpty());
+
+        // String nome = "Alura"; // Forma correta de inicialização -> sintaxe object literal string é uma classe
         // String outro = new String("Alura"); // Má pratica
         // Uma String é imutavel ao ser inicializada para alterações necessário a criação de uma nova string.
 
-        String outra = nome.replace("A", "a");
-        // nome.toLowerCase();
+        // System.out.println(nome.length());
 
-        System.out.println(nome);
-        System.out.println(outra);
+        // for(int i = 0; i < nome.length(); i++){
+        //    System.out.println(nome.charAt(i));
+        // }
+
+        // String sub = nome.substring(1);
+        // System.out.println(sub);
+
+        // char c = 'A';
+        // char d = 'a';
+        // String outra = nome.replace(c, d);
+
+        // char c = nome.charAt(2);
+        // System.out.println(c);
+
+        //int pos = nome.indexOf("ur");
+        // System.out.println(pos);
+
+        // String é um sequencia de caracter ""
+        // Char é um unico caracter ''
+        // String outra = nome.replace("A", "a");
+        // nome.toLowerCase();
+        // String outra = nome.toUpperCase();
+
+        // System.out.println(nome);
+        // System.out.println(outra);
     }
 }
