2c28eb793df68830f1def822432b39712032f0f7
Luiz Felipe <lffm1994@gmail.com>
2023-01-18 22:33:51 -0300

    Adapter para arrays

---
+++ src/teste/Teste.java
@@ -0,0 +1,24 @@
+package teste;
+
+import modelo.Conta;
+import modelo.ContaCorrente;
+import modelo.GuardadorDeContas;
+
+public class Teste {
+    public static void main(String[] args) {
+        GuardadorDeContas guardador = new GuardadorDeContas();
+
+        Conta cc = new ContaCorrente(22, 11);
+        guardador.adiciona(cc);
+
+        Conta cc2 = new ContaCorrente(22, 22);
+        guardador.adiciona(cc2);
+
+        int tamanho = guardador.getQuantidadeDeElementos();
+        System.out.println(tamanho);
+
+        Conta ref = guardador.getReferencia(0);
+        System.out.println(ref.getNumero());
+
+    }
+}
