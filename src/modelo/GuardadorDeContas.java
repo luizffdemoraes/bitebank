d4cec55c11012fc791233eecf605a2932e0a9dbf
Luiz Felipe <lffm1994@gmail.com>
2023-01-18 22:35:46 -0300

    Adapter para arrays

--- src/modelo/GuardadorDeContas.java
+++ src/modelo/GuardadorDeContas.java
@@ -1,2 +1,24 @@
-package modelo;public class GuardadorDeContas {
+package modelo;
+
+public class GuardadorDeContas {
+    private Conta[] referencias;
+    private int posicaoLivre;
+
+    public GuardadorDeContas() {
+        this.referencias = new Conta[10];
+        this.posicaoLivre = 0;
+    }
+
+    public void adiciona(Conta ref) {
+        this.referencias[this.posicaoLivre] = ref;
+        this.posicaoLivre++;
+    }
+
+    public int getQuantidadeDeElementos() {
+        return posicaoLivre;
+    }
+
+    public Conta getReferencia(int posicao) {
+        return this.referencias[posicao];
+    }
 }
